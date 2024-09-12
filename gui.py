af5ad509fa68061bb2d5e0c609ab705e34c187d4
dg1an3 <dg1an3@github.com>
2024-09-12 17:55:19 -0500

    fixing lint errors

--- gui.py
+++ gui.py
@@ -1,28 +1,58 @@
 #!/usr/bin/env python3
+"""
+Main Copycat GUI program.
+"""
 import argparse
 import logging
 
-from copycat import Copycat, Reporter
-
 import matplotlib.pyplot as plt
 
-plt.style.use('dark_background')
+from copycat import Copycat, Reporter
+
+plt.style.use("dark_background")
+
 
 class SimpleReporter(Reporter):
+    """Reports results from a single run."""
+
     def report_answer(self, answer):
-        print('Answered %s (time %d, final temperature %.1f)' % (
-            answer['answer'], answer['time'], answer['temp'],
-        ))
+        print(
+            f"Answered {answer['answer']:s} (time {answer['time']:d}, "
+            f"final temperature {answer['temp']:.1f})"
+        )
+
 
 def main():
-    logging.basicConfig(level=logging.INFO, format='%(message)s', filename='./output/copycat.log', filemode='w')
+    """
+    Main function for running the Copycat program with GUI.
+
+    Args:
+        --seed (int, optional): Provide a deterministic seed for the RNG.
+    
+    Returns:
+        None
+    """
+
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(message)s",
+        filename="./output/copycat.log",
+        filemode="w",
+    )
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--seed', type=int, default=None, help='Provide a deterministic seed for the RNG.')
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=None,
+        help="Provide a deterministic seed for the RNG.",
+    )
     options = parser.parse_args()
 
     copycat = Copycat(reporter=SimpleReporter(), rng_seed=options.seed, gui=True)
     copycat.runGUI()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- main.py
+++ main.py
@@ -37,39 +37,63 @@ import logging
 
 from copycat import Copycat, Reporter, plot_answers, save_answers
 
+
 class SimpleReporter(Reporter):
     """Reports results from a single run."""
 
     def report_answer(self, answer):
-        """Self-explanatory code."""
-        print('Answered %s (time %d, final temperature %.1f)' % (
-            answer['answer'], answer['time'], answer['temp'],
-        ))
+        print(
+            f"Answered {answer['answer']:s} (time {answer['time']:d}, "
+            f"final temperature {answer['temp']:.1f})"
+        )
 
 
 def main():
     """Program's main entrance point.  Self-explanatory code."""
-    logging.basicConfig(level=logging.INFO, format='%(message)s', filename='./output/copycat.log', filemode='w')
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(message)s",
+        filename="./output/copycat.log",
+        filemode="w",
+    )
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--seed', type=int, default=None, help='Provide a deterministic seed for the RNG.')
-    parser.add_argument('--iterations', type=int, default=1, help='Run the given case this many times.')
-    parser.add_argument('--plot', action='store_true', help='Plot a bar graph of answer distribution')
-    parser.add_argument('--noshow', action='store_true', help='Don\'t display bar graph at end of run')
-    parser.add_argument('initial', type=str, help='A...')
-    parser.add_argument('modified', type=str, help='...is to B...')
-    parser.add_argument('target', type=str, help='...as C is to... what?')
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=None,
+        help="Provide a deterministic seed for the RNG.",
+    )
+    parser.add_argument(
+        "--iterations", type=int, default=1, help="Run the given case this many times."
+    )
+    parser.add_argument(
+        "--plot", action="store_true", help="Plot a bar graph of answer distribution"
+    )
+    parser.add_argument(
+        "--noshow", action="store_true", help="Don't display bar graph at end of run"
+    )
+    parser.add_argument("initial", type=str, help="A...")
+    parser.add_argument("modified", type=str, help="...is to B...")
+    parser.add_argument("target", type=str, help="...as C is to... what?")
     options = parser.parse_args()
 
     copycat = Copycat(reporter=SimpleReporter(), rng_seed=options.seed)
-    answers = copycat.run(options.initial, options.modified, options.target, options.iterations)
+    answers = copycat.run(
+        options.initial, options.modified, options.target, options.iterations
+    )
 
-    for answer, d in sorted(iter(answers.items()), key=lambda kv: kv[1]['avgtemp']):
-        print('%s: %d (avg time %.1f, avg temp %.1f)' % (answer, d['count'], d['avgtime'], d['avgtemp']))
+    for answer, d in sorted(iter(answers.items()), key=lambda kv: kv[1]["avgtemp"]):
+        print(
+            f"{answer}: {d['count']} "
+            f"(avg time {d['avgtime']:.1f}, "
+            f"avg temp {d['avgtemp']:.1f})"
+        )
 
     if options.plot:
         plot_answers(answers, show=not options.noshow)
-    save_answers(answers, 'output/answers.csv')
+    save_answers(answers, "output/answers.csv")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
