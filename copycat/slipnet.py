48d1b0cfb94a1c4027a7f2f187b2f1d40f8e23c7
dg1an3 <dg1an3@github.com>
2024-09-18 08:33:47 -0500

    fixing pylint for slipnet

--- copycat/slipnet.py
+++ copycat/slipnet.py
@@ -1,25 +1,32 @@
+from typing import List
+
+from .randomness import Randomness
 from .slipnode import Slipnode
 from .sliplink import Sliplink
 
 
 class Slipnet(object):
+    number_of_updates: int
+    slipnodes: List[Slipnode]
+    initially_clamped_slipnodes: List[Slipnode]
+
     # pylint: disable=too-many-instance-attributes
     def __init__(self):
         self.__addInitialNodes()
         self.__addInitialLinks()
         self.reset()
 
-    def reset(self):
-        self.numberOfUpdates = 0
+    def reset(self) -> None:
+        self.number_of_updates = 0
         for node in self.slipnodes:
             node.reset()
-        for node in self.initiallyClampedSlipnodes:
+        for node in self.initially_clamped_slipnodes:
             node.clamp_high()
 
-    def update(self, random):
-        self.numberOfUpdates += 1
-        if self.numberOfUpdates == 50:
-            for node in self.initiallyClampedSlipnodes:
+    def update(self, random: Randomness) -> None:
+        self.number_of_updates += 1
+        if self.number_of_updates == 50:
+            for node in self.initially_clamped_slipnodes:
                 node.unclamp()
         for node in self.slipnodes:
             node.update()
@@ -44,72 +51,74 @@ class Slipnet(object):
         # pylint: disable=too-many-statements
         self.slipnodes = []
         self.letters = []
-        for c in 'abcdefghijklmnopqrstuvwxyz':
+        for c in "abcdefghijklmnopqrstuvwxyz":
             slipnode = self.__addNode(c, 10.0)
             self.letters += [slipnode]
         self.numbers = []
-        for c in '12345':
+        for c in "12345":
             slipnode = self.__addNode(c, 30.0)
             self.numbers += [slipnode]
 
         # string positions
-        self.leftmost = self.__addNode('leftmost', 40.0)
-        self.rightmost = self.__addNode('rightmost', 40.0)
-        self.middle = self.__addNode('middle', 40.0)
-        self.single = self.__addNode('single', 40.0)
-        self.whole = self.__addNode('whole', 40.0)
+        self.leftmost = self.__addNode("leftmost", 40.0)
+        self.rightmost = self.__addNode("rightmost", 40.0)
+        self.middle = self.__addNode("middle", 40.0)
+        self.single = self.__addNode("single", 40.0)
+        self.whole = self.__addNode("whole", 40.0)
 
         # alphabetic positions
-        self.first = self.__addNode('first', 60.0)
-        self.last = self.__addNode('last', 60.0)
+        self.first = self.__addNode("first", 60.0)
+        self.last = self.__addNode("last", 60.0)
 
         # directions
-        self.left = self.__addNode('left', 40.0)
-        self.left.codelets += ['top-down-bond-scout--direction']
-        self.left.codelets += ['top-down-group-scout--direction']
-        self.right = self.__addNode('right', 40.0)
-        self.right.codelets += ['top-down-bond-scout--direction']
-        self.right.codelets += ['top-down-group-scout--direction']
+        self.left = self.__addNode("left", 40.0)
+        self.left.codelets += ["top-down-bond-scout--direction"]
+        self.left.codelets += ["top-down-group-scout--direction"]
+        self.right = self.__addNode("right", 40.0)
+        self.right.codelets += ["top-down-bond-scout--direction"]
+        self.right.codelets += ["top-down-group-scout--direction"]
 
         # bond types
-        self.predecessor = self.__addNode('predecessor', 50.0, 60)
-        self.predecessor.codelets += ['top-down-bond-scout--category']
-        self.successor = self.__addNode('successor', 50.0, 60)
-        self.successor.codelets += ['top-down-bond-scout--category']
-        self.sameness = self.__addNode('sameness', 80.0)
-        self.sameness.codelets += ['top-down-bond-scout--category']
+        self.predecessor = self.__addNode("predecessor", 50.0, 60)
+        self.predecessor.codelets += ["top-down-bond-scout--category"]
+        self.successor = self.__addNode("successor", 50.0, 60)
+        self.successor.codelets += ["top-down-bond-scout--category"]
+        self.sameness = self.__addNode("sameness", 80.0)
+        self.sameness.codelets += ["top-down-bond-scout--category"]
 
         # group types
-        self.predecessorGroup = self.__addNode('predecessorGroup', 50.0)
-        self.predecessorGroup.codelets += ['top-down-group-scout--category']
-        self.successorGroup = self.__addNode('successorGroup', 50.0)
-        self.successorGroup.codelets += ['top-down-group-scout--category']
-        self.samenessGroup = self.__addNode('samenessGroup', 80.0)
-        self.samenessGroup.codelets += ['top-down-group-scout--category']
+        self.predecessorGroup = self.__addNode("predecessorGroup", 50.0)
+        self.predecessorGroup.codelets += ["top-down-group-scout--category"]
+        self.successorGroup = self.__addNode("successorGroup", 50.0)
+        self.successorGroup.codelets += ["top-down-group-scout--category"]
+        self.samenessGroup = self.__addNode("samenessGroup", 80.0)
+        self.samenessGroup.codelets += ["top-down-group-scout--category"]
 
         # other relations
-        self.identity = self.__addNode('identity', 90.0)
-        self.opposite = self.__addNode('opposite', 90.0, 80.0)
+        self.identity = self.__addNode("identity", 90.0)
+        self.opposite = self.__addNode("opposite", 90.0, 80.0)
 
         # objects
-        self.letter = self.__addNode('letter', 20.0)
-        self.group = self.__addNode('group', 80.0)
+        self.letter = self.__addNode("letter", 20.0)
+        self.group = self.__addNode("group", 80.0)
 
         # categories
-        self.letterCategory = self.__addNode('letterCategory', 30.0)
-        self.stringPositionCategory = self.__addNode('stringPositionCategory', 70.0)
-        self.stringPositionCategory.codelets += ['top-down-description-scout']
-        self.alphabeticPositionCategory = self.__addNode('alphabeticPositionCategory', 80.0)
-        self.alphabeticPositionCategory.codelets += ['top-down-description-scout']
-        self.directionCategory = self.__addNode('directionCategory', 70.0)
-        self.bondCategory = self.__addNode('bondCategory', 80.0)
-        self.groupCategory = self.__addNode('groupCategory', 80.0)
-        self.length = self.__addNode('length', 60.0)
-        self.objectCategory = self.__addNode('objectCategory', 90.0)
-        self.bondFacet = self.__addNode('bondFacet', 90.0)
+        self.letterCategory = self.__addNode("letterCategory", 30.0)
+        self.stringPositionCategory = self.__addNode("stringPositionCategory", 70.0)
+        self.stringPositionCategory.codelets += ["top-down-description-scout"]
+        self.alphabeticPositionCategory = self.__addNode(
+            "alphabeticPositionCategory", 80.0
+        )
+        self.alphabeticPositionCategory.codelets += ["top-down-description-scout"]
+        self.directionCategory = self.__addNode("directionCategory", 70.0)
+        self.bondCategory = self.__addNode("bondCategory", 80.0)
+        self.groupCategory = self.__addNode("groupCategory", 80.0)
+        self.length = self.__addNode("length", 60.0)
+        self.objectCategory = self.__addNode("objectCategory", 90.0)
+        self.bondFacet = self.__addNode("bondFacet", 90.0)
 
         # some factors are considered "very relevant" a priori
-        self.initiallyClampedSlipnodes = [
+        self.initially_clamped_slipnodes = [
             self.letterCategory,
             self.stringPositionCategory,
         ]
@@ -171,13 +180,31 @@ class Slipnet(object):
         for a, b in links:
             self.__addInstanceLink(a, b)
         # link bonds to their groups
-        self.__addNonSlipLink(self.sameness, self.samenessGroup, label=self.groupCategory, length=30.0)
-        self.__addNonSlipLink(self.successor, self.successorGroup, label=self.groupCategory, length=60.0)
-        self.__addNonSlipLink(self.predecessor, self.predecessorGroup, label=self.groupCategory, length=60.0)
+        self.__addNonSlipLink(
+            self.sameness, self.samenessGroup, label=self.groupCategory, length=30.0
+        )
+        self.__addNonSlipLink(
+            self.successor, self.successorGroup, label=self.groupCategory, length=60.0
+        )
+        self.__addNonSlipLink(
+            self.predecessor,
+            self.predecessorGroup,
+            label=self.groupCategory,
+            length=60.0,
+        )
         # link bond groups to their bonds
-        self.__addNonSlipLink(self.samenessGroup, self.sameness, label=self.bondCategory, length=90.0)
-        self.__addNonSlipLink(self.successorGroup, self.successor, label=self.bondCategory, length=90.0)
-        self.__addNonSlipLink(self.predecessorGroup, self.predecessor, label=self.bondCategory, length=90.0)
+        self.__addNonSlipLink(
+            self.samenessGroup, self.sameness, label=self.bondCategory, length=90.0
+        )
+        self.__addNonSlipLink(
+            self.successorGroup, self.successor, label=self.bondCategory, length=90.0
+        )
+        self.__addNonSlipLink(
+            self.predecessorGroup,
+            self.predecessor,
+            label=self.bondCategory,
+            length=90.0,
+        )
         # letter category to length
         self.__addSlipLink(self.letterCategory, self.length, length=95.0)
         self.__addSlipLink(self.length, self.letterCategory, length=95.0)
@@ -215,19 +242,19 @@ class Slipnet(object):
         self.__addNonSlipLink(destination, source, length=length)
 
     def __addCategoryLink(self, source, destination, length):
-        #noinspection PyArgumentEqualDefault
+        # noinspection PyArgumentEqualDefault
         link = self.__addLink(source, destination, None, length)
         source.category_links += [link]
 
     def __addInstanceLink(self, source, destination, length=100.0):
         categoryLength = source.conceptual_depth - destination.conceptual_depth
         self.__addCategoryLink(destination, source, categoryLength)
-        #noinspection PyArgumentEqualDefault
+        # noinspection PyArgumentEqualDefault
         link = self.__addLink(source, destination, None, length)
         source.instance_links += [link]
 
     def __addPropertyLink(self, source, destination, length):
-        #noinspection PyArgumentEqualDefault
+        # noinspection PyArgumentEqualDefault
         link = self.__addLink(source, destination, None, length)
         source.property_links += [link]
 
